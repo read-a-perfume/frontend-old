548def08c3ab65bc0597b1fa8f6afd17d02ea219
YoungkeunAhn <tosan2094@gmail.com>
2023-04-29 16:22:00 +0900

    add: CRA

---
+++ config-overrides.js
@@ -0,0 +1,13 @@
+const {
+  useBabelRc,
+  removeModuleScopePlugin,
+  override,
+  addWebpackAlias,
+} = require('customize-cra')
+const path = require('path')
+
+module.exports = override(
+  useBabelRc(),
+  removeModuleScopePlugin(),
+  addWebpackAlias({ '@': path.resolve(__dirname, 'src') })
+)
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Test from './pages/test/Test'
+
+function App() {
+  return (
+    <div className='App'>
+      <Test />
+    </div>
+  )
+}
+
+export default App
