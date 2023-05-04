23240c7afc89aa3b7073cff149b2e6f6c3067280
djatjdwns <djatjdwns28@gmail.com>
2023-05-04 15:34:17 +0900

    feat : 프로젝트 eslint prettier설정 밋 커밋메세지 템플릿 추가 프로젝트 기초 설정중 ...

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    plugins: [
+      '@typescript-eslint',
+      'react-hooks'
+    ],
+    env: {
+      'browser': true,
+      'es2021': true,
+    },
+    extends: [
+      // 'airbnb-base', // or airbnb
+      'plugin:react/recommended', // eslint-plugin-react 설치한 경우
+      'plugin:jsx-a11y/recommended', // eslint-plugin-jsx-a11y 설치한 경우
+      'plugin:import/errors', // eslint-plugin-import 설치한 경우
+      'plugin:import/warnings', // eslint-plugin-import 설치한 경우
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended',
+      'prettier',
+    ],
+    rules: {
+      'prettier/prettier': 0,
+      'react/jsx-filename-extension': [
+        'warn',
+        {
+          'extensions': ['.tsx']
+        }
+      ],
+      "react/react-in-jsx-scope": "off",
+    },
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: [
+            '.js',
+            '.jsx',
+            '.ts',
+            '.tsx',
+          ],
+        },
+      },
+    },
+  };
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import './App.css'
-import Test from './pages/test/Test'
+import './App.css';
+import Test from './pages/test/Test';
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Test />
+      <div className="app">
+        ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
