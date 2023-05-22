8eff3e96f9334d8fab56a56396ce7c2da3ba05ea
dulgi <development@farmos.co.kr>
2023-05-22 18:01:28 +0900

    etc : 로그인 열심히 수정중 ..

--- src/components/sign-in/SignInForm.tsx
+++ src/components/sign-in/SignInForm.tsx
@@ -1,13 +1,44 @@
-import {Dialog, Tab, Tabs} from '@mui/material'
+import {Box, Button, Dialog, Grid, Tab, Tabs, TextField, Typography} from '@mui/material'
 import {
   DialogGridContainer,
   DialogGrid,
   SignInDialogContent,
   DialogGridTitle,
   DialogGridSubTitle,
+  TabBox,
+  SignInTextField,
+  TextFieldLabel,
+  LoginButton,
 } from './SignInStyle'
+import { ChangeEventHandler, SyntheticEvent, useCallback, useState } from 'react'
+
+interface TabItemProps {
+  children?: React.ReactNode
+  index: number
+  value: number
+}
+
+function TabItem(props: TabItemProps) {
+  const {children, value, index, ...other} = props
+
+  return (
+    <Box sx={{width:'100%'}} hidden={value !== index}>{}
+    {value === index && (
+      <Typography>{children}</Typography>
+    )}
+    </Box>
+  )
+}
 
 export const SignInForm = () => {
+  const [value, setValue] = useState(0);
+  const [email, setEmail] = useState('');
+  const handleChange = useCallback((event: SyntheticEvent, newValue: number) => {
+    setValue(newValue)
+  },[]);
+  const onChangeId = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value)
+  },[])
   return (
     <SignInDialogContent>
       <DialogGridContainer container spacing={1}>
@@ -18,10 +49,21 @@ export const SignInForm = () => {
           </DialogGridSubTitle>
         </DialogGrid>
         <DialogGrid item>
-          <Tabs>
-            <Tab label="개인용"></Tab>
-            <Tab label="기업용"></Tab>
-          </Tabs>
+          <TabBox>
+            <Tabs variant='fullWidth' textColor='inherit' value={value} onChange={handleChange} TabIndicatorProps={{style: {background:'black'}}}>
+              <Tab label="개인용"></Tab>
+              <Tab label="기업용"></Tab>
+            </Tabs>
+          </TabBox>
+          <TabItem value={value} index={0}>
+            <Box sx={{py: 1}}>
+              <SignInTextField onChange={onChangeId} variant="outlined"label={<TextFieldLabel>아이디</TextFieldLabel> } size='small'/>
+              <SignInTextField variant="outlined" label={<TextFieldLabel>패스워드</TextFieldLabel>} size='small' type="password"/>
+              <LoginButton variant='contained' customColor='#F8DB52'>로그인</LoginButton>
+              <LoginButton variant='outlined' customColor='white' sx={{mt:1}}>Google 로그인</LoginButton>
+            </Box>
+          </TabItem>
+          <TabItem value={value} index={1}>기업 로그인 폼</TabItem>
         </DialogGrid>
       </DialogGridContainer>
     </SignInDialogContent>
--- src/components/sign-in/SignInStyle.ts
+++ src/components/sign-in/SignInStyle.ts
@@ -1,13 +1,22 @@
+import { BorderColor, Label } from '@mui/icons-material'
 import {
   Box,
+  Button,
   Dialog,
   DialogContent,
   FormGroup,
   Grid,
+  makeStyles,
   Paper,
   styled,
+  TextField,
   Typography,
 } from '@mui/material'
+
+interface LoginButtonProps {
+  customColor: '#F8DB52' | 'white'
+}
+
 export const SignInDialog = styled(Dialog)(({theme}) => ({
   maxWidth: 420,
   margin: '0 auto',
@@ -17,7 +26,9 @@ export const SignInDialogContent = styled(DialogContent)(() => ({
 }))
 
 export const DialogGridContainer = styled(Grid)(() => ({}))
-export const DialogGrid = styled(Grid)(() => ({}))
+export const DialogGrid = styled(Grid)(() => ({
+  width: '100%'
+}))
 export const DialogGridTitle = styled(Typography)(() => ({
   fontSize: '22px',
   color: '#131313',
@@ -25,4 +36,25 @@ export const DialogGridTitle = styled(Typography)(() => ({
 export const DialogGridSubTitle = styled(Typography)(() => ({
   fontSize: '20px',
   fontWeight: 'bold',
+}))
+export const TabBox = styled(Box)(() => ({
+  borderBottom: 1,
+  borderColor: 'divider'
+}))
+export const SignInTextField = styled(TextField)(() => ({
+  width: '100%',
+  marginBottom: '10px',
+}))
+export const TextFieldLabel = styled('label')(() => ({
+  fontSize: '14px'
+}))
+export const LoginButton = styled(Button)<LoginButtonProps>(({...props}) => ({
+  boxShadow: 'none',
+  width: '100%',
+  background: props.customColor,
+  color: 'black',
+  borderColor: '#EFEFEF',
+  '&:hover': {
+    borderColor: '#EFEFEF'
+  }
 }))
