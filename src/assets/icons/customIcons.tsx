b35c805e68458acbfd91b95f55ac028629b878b2
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-06-17 12:35:44 +0900

    feat: add custom Icons

---
+++ src/assets/icons/customIcons.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {createSvgIcon} from '@mui/material/utils'
+
+const CommentIcon = createSvgIcon(
+  <path
+    d="M10.5716 16.2675C15.6207 16.2675 19.7144 12.6355 19.7144 8.50681C19.7144 4.37815 15.6207 1.31787 10.5716 1.31787C5.52243 1.31787 1.42871 4.6646 1.42871 8.79326C1.42871 10.4047 2.05271 11.8978 3.11443 13.1177L2.00014 18.1842L6.47557 15.4779C7.77243 16.0041 9.16535 16.2726 10.5716 16.2675Z"
+    fill="#E7E7E7"
+    stroke="#E7E7E7"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  />,
+  'CommentIcon',
+)
+
+const CustomIcons = {
+  CommentIcon
+};
+
+export default CustomIcons;
