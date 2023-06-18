ea53b2fe527543669ba810e4cb91bee3e8201e1b
YoungkeunAhn <tosan2094@gmail.com>
2023-06-18 22:53:23 +0900

    fix: optimazation

--- src/pages/sign-up/SignUp.tsx
+++ src/pages/sign-up/SignUp.tsx
@@ -1,4 +1,3 @@
-import UserTypeSelect from '@components/user-type-select-box/UserTypeSelect'
 import {Box, styled} from '@mui/material'
 import SignUpForm from '@components/sign-up-form/SignUpForm'
 import {useState} from 'react'
@@ -7,10 +6,6 @@ export type SignUpType = 'personal' | 'enterprise' | ''
 
 export default function SignUp() {
   const [currentType, setCurrentType] = useState<SignUpType>('')
-
-  // const onChangeType = (type: SignUpType) => {
-  //   setCurrentType(type)
-  // }
 
   const Container = styled(Box)(() => ({
     height: '100vh',
