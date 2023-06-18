7585671a3bd9e599b2633a595828ea36b3c29cc5
YoungkeunAhn <tosan2094@gmail.com>
2023-06-18 21:27:57 +0900

    feat: 로그인 기능 추가

--- src/components/sign-in/SignInForm.tsx
+++ src/components/sign-in/SignInForm.tsx
@@ -1,4 +1,13 @@
-import {Box, Button, Dialog, Grid, Tab, Tabs, TextField, Typography} from '@mui/material'
+import {
+  Box,
+  Button,
+  Dialog,
+  Grid,
+  Tab,
+  Tabs,
+  TextField,
+  Typography,
+} from '@mui/material'
 import {
   DialogGridContainer,
   DialogGrid,
@@ -10,7 +19,9 @@ import {
   TextFieldLabel,
   LoginButton,
 } from './SignInStyle'
-import { ChangeEventHandler, SyntheticEvent, useCallback, useState } from 'react'
+import {ChangeEventHandler, SyntheticEvent, useCallback, useState} from 'react'
+import axios from 'axios'
+import {BASE_URL} from '@api/Apiconfig'
 
 interface TabItemProps {
   children?: React.ReactNode
@@ -22,26 +33,46 @@ function TabItem(props: TabItemProps) {
   const {children, value, index, ...other} = props
 
   return (
-    <Box sx={{width:'100%'}} hidden={value !== index}>{}
-    {value === index && (
-      <Typography>{children}</Typography>
-    )}
+    <Box sx={{width: '100%'}} hidden={value !== index}>
+      {}
+      {value === index && <Typography>{children}</Typography>}
     </Box>
   )
 }
 
 export const SignInForm = () => {
-  const [value, setValue] = useState(0);
-  const [email, setEmail] = useState('');
-  const handleChange = useCallback((event: SyntheticEvent, newValue: number) => {
-    setValue(newValue)
-  },[]);
-  const onChangeId = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.target.value)
-  },[])
-  const onChangePw = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.target.value)
-  },[])
+  const [value, setValue] = useState<number>(0)
+  const [username, setUsername] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const handleChange = useCallback(
+    (event: SyntheticEvent, newValue: number) => {
+      setValue(newValue)
+    },
+    [],
+  )
+  const onChangeId = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setUsername(event.target.value)
+    },
+    [],
+  )
+  const onChangePw = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setPassword(event.target.value)
+    },
+    [],
+  )
+
+  const onLogin = async () => {
+    try {
+      const {data} = await axios.post(BASE_URL + '/api/v1/login', {
+        username: username,
+        password: value,
+      })
+    } catch (err) {
+      console.error(err)
+    }
+  }
   return (
     <SignInDialogContent>
       <DialogGridContainer container spacing={1}>
@@ -53,20 +84,49 @@ export const SignInForm = () => {
         </DialogGrid>
         <DialogGrid item>
           <TabBox>
-            <Tabs variant='fullWidth' textColor='inherit' value={value} onChange={handleChange} TabIndicatorProps={{style: {background:'black'}}}>
+            <Tabs
+              variant="fullWidth"
+              textColor="inherit"
+              value={value}
+              onChange={handleChange}
+              TabIndicatorProps={{style: {background: 'black'}}}
+            >
               <Tab label="개인용"></Tab>
               <Tab label="기업용"></Tab>
             </Tabs>
           </TabBox>
           <TabItem value={value} index={0}>
             <Box sx={{py: 1}}>
-              <SignInTextField onChange={onChangeId} variant="outlined"label={<TextFieldLabel>아이디</TextFieldLabel> } size='small'/>
-              <SignInTextField onChange={onChangePw} variant="outlined" label={<TextFieldLabel>패스워드</TextFieldLabel>} size='small' type="password"/>
-              <LoginButton variant='contained' customColor='#F8DB52'>로그인</LoginButton>
-              <LoginButton variant='outlined' customColor='white' sx={{mt:1}}>Google 로그인</LoginButton>
+              <SignInTextField
+                onChange={onChangeId}
+                variant="outlined"
+                value={username}
+                label={<TextFieldLabel>아이디</TextFieldLabel>}
+                size="small"
+              />
+              <SignInTextField
+                onChange={onChangePw}
+                variant="outlined"
+                label={<TextFieldLabel>패스워드</TextFieldLabel>}
+                value={password}
+                size="small"
+                type="password"
+              />
+              <LoginButton
+                variant="contained"
+                customColor="#F8DB52"
+                onClick={onLogin}
+              >
+                로그인
+              </LoginButton>
+              <LoginButton variant="outlined" customColor="white" sx={{mt: 1}}>
+                Google 로그인
+              </LoginButton>
             </Box>
           </TabItem>
-          <TabItem value={value} index={1}>기업 로그인 폼</TabItem>
+          <TabItem value={value} index={1}>
+            기업 로그인 폼
+          </TabItem>
         </DialogGrid>
       </DialogGridContainer>
     </SignInDialogContent>
