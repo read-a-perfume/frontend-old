14d9958bead4280f347d6b072ce87dc8e50d4477
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-06-29 15:04:18 +0900

    fix: change findId page path

--- src/components/main/LoginModal/LoginModal.tsx
+++ src/components/main/LoginModal/LoginModal.tsx
@@ -1,11 +1,11 @@
 import {useState} from 'react'
-import FindId from './FindID/FindId'
-import IdResult from './FindID/IdResult'
 import FindPassword from './FindPassword/FindPassword'
 import SendEmail from './FindPassword/SendEmail'
 import LoginLayout from './layout/Login'
 import {ModalProps} from './layout/LoginModal.interface'
 import {ModalLayout, ModalStyle} from './layout/LoginModal.style'
+import FindId from './FindId/FindId'
+import IdResult from './FindId/IdResult'
 
 function LoginModal({isOpen, setIsOpen}: ModalProps) {
   const [condition, setCondition] = useState<string>('')
