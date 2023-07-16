c045d4960cb20894ff0a276eb47796926544e690
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-07-16 20:52:05 +0900

    feat: add route, useAddHypen hook, Brand page

---
+++ src/components/brand/BrandForm.tsx
@@ -0,0 +1,87 @@
+import FlexBox from '@components/layout/FlexBox'
+import {Label} from '@components/main/LoginModal/FindPassword/FindPassword.style'
+import {FormControl} from '@mui/material'
+import {
+  AlertSpan,
+  CategoryTitle,
+  EditProfileButton,
+  FormBlock,
+  Input,
+  Profile,
+  Styledswitch,
+} from '@pages/brand/Brand.style'
+import React from 'react'
+
+const Form = ({ secondBlock, thirdBlock } : {
+  secondBlock : React.RefObject<HTMLDivElement>;
+  thirdBlock : React.RefObject<HTMLDivElement>;
+}) => {
+  // const [image, setImage] = useState<string>('')
+  const image = ''
+
+  return (
+    <FormBlock>
+      <FlexBox alignItems="center" style={{gap: '27px'}}>
+        <Profile>{image && <img src={image} alt="profile" />}</Profile>
+        <EditProfileButton>프로필 변경</EditProfileButton>
+      </FlexBox>
+      <CategoryTitle marginTop="48px">브랜드 기본 정보</CategoryTitle>
+      <FlexBox style={{gap: '31px', marginBottom: '32px'}}>
+        <FormControl>
+          <Label>브랜드명</Label>
+          <Input name="name" />
+        </FormControl>
+        <FormControl>
+          <Label>웹 사이트 URL</Label>
+          <Input name="url" />
+        </FormControl>
+      </FlexBox>
+      <FormControl style={{marginBottom: '32px'}}>
+        <Label>브랜드 소개</Label>
+        <Input full name="url" />
+      </FormControl>
+      <FlexBox style={{gap: '31px'}}>
+        <FormControl>
+          <Label>기업명</Label>
+          <Input name="businessName" />
+        </FormControl>
+        <FormControl>
+          <Label>사용자등록번호</Label>
+          <Input name="businessNumber" />
+        </FormControl>
+      </FlexBox>
+      <div ref={secondBlock}>
+        <CategoryTitle marginTop="78px">관리자 계정 관리</CategoryTitle>
+        <FlexBox style={{gap: '31px', marginBottom: '32px'}}>
+          <FormControl>
+            <Label>아이디</Label>
+            <Input name="id" />
+          </FormControl>
+          <FormControl>
+            <Label>비밀번호</Label>
+            <Input name="password" />
+          </FormControl>
+        </FlexBox>
+        <FlexBox style={{gap: '31px'}}>
+          <FormControl>
+            <Label>기업용 이메일</Label>
+            <Input name="businessEmail" />
+          </FormControl>
+          <FormControl>
+            <Label>휴대전화</Label>
+            <Input name="phone" />
+          </FormControl>
+        </FlexBox>
+      </div>
+      <div ref={thirdBlock}>
+        <CategoryTitle marginTop="88px">알림 설정</CategoryTitle>
+        <FlexBox alignItems="center">
+          <AlertSpan>브랜드 제품에 대한 리뷰 알림</AlertSpan>
+          <Styledswitch defaultChecked />
+        </FlexBox>
+      </div>
+    </FormBlock>
+  )
+}
+
+export default Form
