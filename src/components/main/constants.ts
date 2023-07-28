b3cdb02601749445773a733f06aa2445c044c0a8
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-07-28 17:27:24 +0900

    feat: add constants file for mock data

---
+++ src/components/main/constants.ts
@@ -0,0 +1,73 @@
+export const noteData = [
+  {
+    name: 'Fruity',
+    image: '',
+    cover_image: '',
+    content: '',
+  },
+  {
+    name: 'Woody',
+    image: '',
+    cover_image: '/images/note_bg.png',
+    content: `
+        나무 향을 의미하며,
+        건조하고 성숙한 느낌을 전달합니다.`,
+  },
+  {
+    name: 'Green',
+    image: '',
+    cover_image: '',
+    content: '',
+  },
+  {name: 'Spicy', image: '', cover_image: '', content: ''},
+  {name: 'Animal', image: '', cover_image: '', content: ''},
+  {name: 'Citrus', image: '', cover_image: '', content: ''},
+  {name: 'Light', image: '', cover_image: '', content: ''},
+  {name: 'Musk', image: '', cover_image: '', content: ''},
+  {name: 'Heavy', image: '', cover_image: '', content: ''},
+]
+
+export const magazineData = [
+  {
+    image: '/images/banner.png',
+    title: '심플한 디자인이 돋보이는 전설적인 향수 N°5 빠르펭',
+    content:
+      '심플한 디자인이 돋보이는 전설적인 향수 N°5 빠르펭. 시간을 초월하여 사랑받는 아이콘으로 여성의 진정한 아름다움을 표현합니다. 알데하이드 플로랄 부케는 "여성의 향기를 담은 여성미의 진수"를 만들어 달라고 부탁한 가브리엘...',
+    hash: [['플로랄'], ['여름신작'], ['슬로우 썸머']],
+  },
+  {
+    image: '/images/banner.png',
+    title: '심플한 디자인이 돋보이는 전설적인 향수 N°5 빠르펭',
+    content:
+      '심플한 디자인이 돋보이는 전설적인 향수 N°5 빠르펭. 시간을 초월하여 사랑받는 아이콘으로 여성의 진정한 아름다움을 표현합니다. 알데하이드 플로랄 부케는 "여성의 향기를 담은 여성미의 진수"를 만들어 달라고 부탁한 가브리엘...',
+    hash: [['플로랄'], ['여름신작'], ['슬로우 썸머']],
+  },
+  {
+    image: '/images/banner.png',
+    title: '심플한 디자인이 돋보이는 전설적인 향수 N°5 빠르펭',
+    content:
+      '심플한 디자인이 돋보이는 전설적인 향수 N°5 빠르펭. 시간을 초월하여 사랑받는 아이콘으로 여성의 진정한 아름다움을 표현합니다. 알데하이드 플로랄 부케는 "여성의 향기를 담은 여성미의 진수"를 만들어 달라고 부탁한 가브리엘...',
+    hash: [['플로랄'], ['여름신작'], ['슬로우 썸머']],
+  },
+  {
+    image: '/images/banner.png',
+    title: '심플한 디자인이 돋보이는 전설적인 향수 N°5 빠르펭',
+    content:
+      '심플한 디자인이 돋보이는 전설적인 향수 N°5 빠르펭. 시간을 초월하여 사랑받는 아이콘으로 여성의 진정한 아름다움을 표현합니다. 알데하이드 플로랄 부케는 "여성의 향기를 담은 여성미의 진수"를 만들어 달라고 부탁한 가브리엘...',
+    hash: [['플로랄'], ['여름신작'], ['슬로우 썸머']],
+  },
+  {
+    image: '/images/banner.png',
+    title: '심플한 디자인이 돋보이는 전설적인 향수 N°5 빠르펭',
+    content:
+      '심플한 디자인이 돋보이는 전설적인 향수 N°5 빠르펭. 시간을 초월하여 사랑받는 아이콘으로 여성의 진정한 아름다움을 표현합니다. 알데하이드 플로랄 부케는 "여성의 향기를 담은 여성미의 진수"를 만들어 달라고 부탁한 가브리엘...',
+    hash: [['플로랄'], ['여름신작'], ['슬로우 썸머']],
+  },
+  {
+    image: '/images/banner.png',
+    title: '심플한 디자인이 돋보이는 전설적인 향수 N°5 빠르펭',
+    content:
+      '심플한 디자인이 돋보이는 전설적인 향수 N°5 빠르펭. 시간을 초월하여 사랑받는 아이콘으로 여성의 진정한 아름다움을 표현합니다. 알데하이드 플로랄 부케는 "여성의 향기를 담은 여성미의 진수"를 만들어 달라고 부탁한 가브리엘...',
+    hash: [['플로랄'], ['여름신작'], ['슬로우 썸머']],
+  },
+]
